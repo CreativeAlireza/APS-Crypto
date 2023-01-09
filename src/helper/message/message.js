582a7aca96b43779c66f81a475f9d56c0a4cca64
Alireza Rahimi <ar.dvlpr@outlook.com>
2023-01-09 22:55:21 +0330

    add message

---
+++ src/helper/message/message.js
@@ -0,0 +1,24 @@
+export function message(message, page = "", time = 0, title = 'Message') {
+
+    const modal = document.querySelector('.modal');
+    const modalBody = document.querySelector('.modal-body');
+    const modalCloseBtn = document.querySelector('.modal-close-btn');
+    const modalFooter = document.querySelector('.modal-footer');
+    const modalTitle = document.querySelector('.modal-title');
+    const modalBackdrop = document.querySelector('.modal-backdrop');
+
+    modalBody.textContent = message;
+    modalTitle.textContent = title;
+
+    if(page){
+        modalCloseBtn.remove();
+        modalFooter.remove();
+        
+        setTimeout(() => {
+            modalBackdrop.remove();
+            modal.classList.remove('show');
+            page();
+        }, time);
+    }
+}
+
