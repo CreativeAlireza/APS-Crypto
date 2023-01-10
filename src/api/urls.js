0fc2f37954aa0f4e4ceba9549cb8ae111ac1f975
Alireza Rahimi <ar.dvlpr@outlook.com>
2023-01-10 23:36:49 +0330

    add top 3 markect cap to urls

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,1 +1,2 @@
 export const COINS_MARKETS = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=gecko_desc&per_page=250&page=1&sparkline=false`;
+export const TOP3_MARKETS = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=gecko_desc&per_page=3&page=1&sparkline=false`;
