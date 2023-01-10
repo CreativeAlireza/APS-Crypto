5032256b3591e037cf96b29a45df5771523ec31d
Alireza Rahimi <ar.dvlpr@outlook.com>
2023-01-10 20:08:18 +0330

    add coinsList to home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,9 @@
 import { topCharts } from "../components/charts/topCharts.js";
+import { coinsList } from "../components/coinsList/coinsList.js";
 import { clearMain } from "../ui/domActions/domActions.js";
 
 export function homeUI() {
     clearMain();
     topCharts();
+    coinsList();
 }
