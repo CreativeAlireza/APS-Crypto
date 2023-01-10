3c6116fedea8e0d580e215468c11c5ad9e235698
Alireza Rahimi <ar.dvlpr@outlook.com>
2023-01-10 18:26:13 +0330

    add home to nav

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
 import {NavPanelActions} from "./ui/actions/actions.js";
 import { logInUI } from "./pages/login.js";
+import { homeUI } from "./pages/home.js";
 
 const navList = document.querySelector('.nav-list');
 
-
-
 NavPanelActions();
+homeUI();
 
 navList.addEventListener('click', (e) => {
     switch (e.target.innerText) {
         case "Sign In":
             logInUI();
             break;
+        case "Home":
+            homeUI();
+            break;
         default:
+            homeUI();
     }
 });
