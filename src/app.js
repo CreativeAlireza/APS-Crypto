218cc097045c2b4039ba6f1588662993fa92f97a
Alireza Rahimi <ar.dvlpr@outlook.com>
2023-01-12 21:29:32 +0330

    add about to nav panel and updated it

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import { NavPanelActions } from "./ui/actions/actions.js";
 import { logInUI } from "./pages/login.js";
 import { homeUI } from "./pages/home.js";
 import { portfolioUI } from "./pages/portfolio.js";
+import { aboutUI } from "./pages/about.js";
 
 const navList = document.querySelector('.nav-list');
 
@@ -10,14 +11,17 @@ homeUI();
 
 navList.addEventListener('click', (e) => {
     switch (e.target.innerText) {
-        case "Sign In":
-            logInUI();
-            break;
         case "Home":
             homeUI();
             break;
         case "Portfolio":
             portfolioUI();
+            break;
+        case "About":
+            aboutUI();
+            break;
+        case "Sign In":
+            logInUI();
             break;
         default:
             homeUI();
