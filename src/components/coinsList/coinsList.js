325aed3a8f438c23868ab928b1935fc21f0cce09
Alireza Rahimi <ar.dvlpr@outlook.com>
2023-01-13 00:01:08 +0330

    improve coinsList css method

--- src/components/coinsList/coinsList.js
+++ src/components/coinsList/coinsList.js
@@ -2,6 +2,8 @@ import { COINS_MARKETS } from "../../api/urls.js";
 import { fetchData } from "../../api/fetchAPI.js";
 
 export async function coinsList(){
+    const sheet = new CSSStyleSheet();
+
     const main = document.querySelector('.main');
     
     const coins = await fetchData(COINS_MARKETS);
@@ -9,17 +11,17 @@ export async function coinsList(){
     const element = `
         <div
             class="coinsList-row row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 text-center d-flex justify-content-around align-items-center">
-            <div class="col-lg-11 col-md-11 col-sm-12 table-responsive">
+            <div class="col-10 col-lg-11 col-md-11 col-sm-12 table-responsive">
                 <table class="table caption-top">
                     <caption class="fs-5">List of Coins</caption>
                     <thead class="table-dark">
                         <tr>
                         <th scope="col" class="rounded-start"></th>
                         <th scope="col">#</th>
-                        <th scope="col" style="width: 1rem !important">Name</th>
-                        <th scope="col">Symbol</th>
-                        <th scope="col">Price</th>
-                        <th scope="col" class="py-2 px-0 rounded-end">Market Cap</th>
+                        <th scope="col" class="coin-name">Name</th>
+                        <th scope="col" class="symbol">Symbol</th>
+                        <th scope="col" class="price">Price</th>
+                        <th scope="col" class="mcap py-2 px-0 rounded-end">Market Cap</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -31,15 +33,15 @@ export async function coinsList(){
                                 <td class="d-flex justify-content-start">
                                     <img 
                                         src="${el.image}" 
-                                        style="width: 2rem;height: 2rem;"
-                                        alt="${el.name}">
+                                        alt="${el.name}"
+                                        class="coin-image">
                                     <div class="d-flex align-items-center ps-3">
                                         ${el.name}
                                     </div>
                                 </td>
-                                <td>${el.symbol.toUpperCase()}</td>
+                                <td class="symbol">${el.symbol.toUpperCase()}</td>
                                 <td>${el.current_price}</td>
-                                <td>${el.market_cap}</td>
+                                <td class="mcap">${el.market_cap}</td>
                             </tr>
                         `
                     )).replaceAll(",", "")}
@@ -50,4 +52,33 @@ export async function coinsList(){
     `;
 
     main.insertAdjacentHTML('beforeend', element);
+
+    sheet.replaceSync(`
+        .coin-name {
+            width: 1rem !important
+        }
+
+        .coin-image {
+            width: 2rem;
+            height: 2rem;
+        }
+        
+        @media (width < 600px) {
+            .mcap {
+                display: none;
+            }
+            .price{
+                border-top-right-radius: 0.375rem;
+                border-bottom-right-radius: 0.375rem;
+            }
+        }
+        
+        @media (width < 550px) {
+            .symbol {
+                display: none;
+            }
+        }
+    `);
+
+    document.adoptedStyleSheets = [sheet];
 }
