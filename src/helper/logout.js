2034ea8636226f2f1fab86d1bd14971c4740b4e0
Alireza Rahimi <ar.dvlpr@outlook.com>
2023-01-13 21:34:57 +0330

    update logout

--- src/helper/logout.js
+++ src/helper/logout.js
@@ -4,9 +4,12 @@ import { saveData } from "./dataStorage.js";
 
 export function logOut() {
     const logoutBtn = document.querySelector('.logout-btn');
+    const navLogout = document.querySelector('.nav-logout');
+
     logoutBtn.addEventListener('click', () => {
         saveData(`userAccess`, "");
         userProfile();
         homeUI();
+        navLogout.remove();
     })
 }
