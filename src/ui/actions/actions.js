9a40ff01f9a396d099d27fa71107cf4a5cf68fac
Alireza Rahimi <ar.dvlpr@outlook.com>
2023-01-13 21:41:02 +0330

    improve navPanelActions

--- src/ui/actions/actions.js
+++ src/ui/actions/actions.js
@@ -4,16 +4,17 @@ const closeNavPanel = document.querySelector('.close-nav-panel');
 const navList = document.querySelector('.nav-list');
 const main = document.querySelector('main');
 
-export function NavPanelActions(){
-    function closePanel(){
-        menu.classList.remove('fadein')
-        menu.classList.add('fadeout');
-    }
-    function openPanel(){
-        menu.classList.remove('d-none', 'fadeout')
-        menu.classList.add('fadein');
-    }
+export function closePanel() {
+    menu.classList.remove('fadein')
+    menu.classList.add('fadeout');
+}
 
+export function openPanel() {
+    menu.classList.remove('d-none', 'fadeout')
+    menu.classList.add('fadein', 'd-flex', 'flex-column', 'justify-content-between');
+}
+
+export function NavPanelActions() {
     main.addEventListener('click', () => closePanel());
     navList.addEventListener('click', () => closePanel());
     closeNavPanel.addEventListener('click', () => closePanel());
